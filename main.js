bca0154747e7a10a7ad4c20ef62e3085433b5505
geritol <gergo.tolnai@gmail.com>
2017-08-27 10:12:18 +0200

    now checking both diagonals

--- main.js
+++ main.js
@@ -8,13 +8,19 @@ const BOARDWIDTH = 300
 const BOARDHEIGHT = 300
 
 // TODO: board class
-var board = []
-for(let i = 0; i < BOARDSIZE; i++){
-  board.push([])
-  for(let j = 0; j < BOARDSIZE; j++){
-    board[i].push("")
+var board
+
+function initBoard(){
+  board = []
+  for(let i = 0; i < BOARDSIZE; i++){
+    board.push([])
+    for(let j = 0; j < BOARDSIZE; j++){
+      board[i].push(" ")
+    }
   }
 }
+
+initBoard()
 
 // draw separating lines
 function drawLines(){
@@ -38,10 +44,11 @@ var moveType = 'x'
 
 document.getElementById('board-tiles').addEventListener("mouseup", function(e){
   addMove(moveType, e.offsetX, e.offsetY)
-  if(checkIfWon()){
+  if(checkIfWon(board)){
     alert(moveType + ' won the game!')
     canvasTiles.clearRect(0,0,BOARDWIDTH, BOARDHEIGHT)
     drawLines()
+    initBoard()
   }
   moveType = moveType === 'x' ? 'y' : 'x'
 })
@@ -68,25 +75,22 @@ valid coordinates provided, x and y should be in the range of the board. Got: x:
 }
 
 
-function checkIfWon(){
+function checkIfWon(board, secondRun){
   function check(rowString){
-    if(rowString.length === CONSECUTIVETILESFORWIN && (rowString.indexOf('x') === -1 || rowString.indexOf('y') === -1)){
+    seekedX = Array(CONSECUTIVETILESFORWIN + 1).join('x')
+    seekedY = Array(CONSECUTIVETILESFORWIN + 1).join('y')
+    if(rowString.indexOf(seekedX) !== -1 || rowString.indexOf(seekedY) !== -1){
       return true
     }
   }
 
   for(let i = 0; i < board.length; i++){
+    // verticall check done by this code on the second run (the list is flipped)
     let resHorizontal = board[i].join('')
-    let resVertical = ''
-    for(let j = 0; j < board[i].length; j++){
-      resVertical += board[j][i]
-    }
-    //console.log('res vertical: ' + resHorizontal)
-    if(check(resVertical) || check(resHorizontal)) return true
+    if(check(resHorizontal)) return true
   }
 
   // check diagonals
-  // TODO: check the other way too
   var diagonalCoords = [0,0]
   for(let i = board.length-1; i > - board.length; i --){
 
@@ -102,12 +106,25 @@ function checkIfWon(){
     for(let j = diagonalCoords[0]; j < board[diagonalCoords[0]].length; j++){
       res += board[diagonalCoords[0]][diagonalCoords[1]]
       diagonalCoords = [diagonalCoords[0] + 1, diagonalCoords[1] + 1]
+      // exit if out of range
       if(!board[diagonalCoords[0]] || !board[diagonalCoords[0]][diagonalCoords[1]]) break
     }
     //console.log(res)
     if(check(res)) return true
+    if(!secondRun) return checkIfWon(transpose(board), true)
   }
   return false
+}
+
+// https://stackoverflow.com/questions/4492678/swap-rows-with-columns-transposition-of-a-matrix-in-javascript/13241545#13241545
+// transpose array and flip its rows horizontally
+function transpose(a) {
+    let transposed = Object.keys(a[0]).map(function(c) {
+        return a.map(function(r) { return r[c]; });
+    });
+    return transposed.map(function(r){
+      return r.reverse()
+    })
 }
 
 // TODO: X and O sizes based on place in the square
